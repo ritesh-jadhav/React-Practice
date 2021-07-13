270b725f30cd5108ef3c53bf835267a113b9184a
ritesh-jadhav <jadhav.ritesh0416@gmail.com>
2021-07-14 01:31:14 +0530

    handling multiple state data for toggle counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,8 +4,10 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
-
-  const toggleCounterHandler = () => {};
+  const show = useSelector(state=> state.showCounter);
+  const toggleCounterHandler = () => {
+    dispatch({ type:'toggle'});
+  };
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
@@ -19,8 +21,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increseHandler}>Increase By 5</button>
